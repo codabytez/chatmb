345d548a83137b2c40bdeb768c5c0d12062101d2
Obinna Chidi <chidiobinna0001@gmail.com>
2023-10-09 08:29:43 +0100

    worked on mobile responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const accordion = document.getElementsByClassName("accordion");
+const accordion = document.getElementsByClassName("accordion-header");
+const scrollToTop = document.querySelector(".scroll-to-top");
 
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
+    // console.log('test')
     this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
   });
 }
+
+scrollToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
